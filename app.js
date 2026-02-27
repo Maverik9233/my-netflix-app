157fdb7ba70535b954e1364adbb8d35bcaaf42e9
Maverik9233 <maverikinter92@gmail.com>
2026-02-27 17:58:07 +0100

    Update PLAYLISTS_URL to new Dropbox link

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PLAYLISTS_URL = 'https://www.dropbox.com/scl/fi/fvxhrlm03y7yyyoaog7k2/Film-e-serie-TV.txt?rlkey=m41lwrfel59cydeh213i5wxu7&st=7iysx6ta&raw=1'; // ← SOSTITUISCI
+const PLAYLISTS_URL = 'https://uc0a8c9a14febdb4f35ec29a436d.dl.dropboxusercontent.com/cd/0/inline/C7vU6SeEQWLbGUWWtgfDcstRU1_xNuEgZ11SWWhv5U7V2ydJ2ovFHYsEyyJ_wMTLNPX6KyQbjKZu2Iio3iV-0b6aUgwEyQmLaSiwgyqrxS9rSePRn9P7v6a-Fns0k8epxa6Lfq48nHTaae2sjr4FScr5/file#'; // ← SOSTITUISCI
 const OMDB_API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=ebab3099';
 
 let allContent = [];          // per ricerca globale
